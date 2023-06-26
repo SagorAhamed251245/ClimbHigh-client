23eb715443ff6c4d22951d941cf7af1da7fb0dbf
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-06-26 18:59:28 +0600

    navbar

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,22 +1,28 @@
-import React from "react";
 import Container from "../../../components/Container/Container";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Container>
-      <div className="w-full" >
+      <div className="w-full text-white">
         {/* top nav start */}
-        <div className="h-16 flex relative">
-          <div className="bg-white h-16 w-4/5"></div>
+        <div className="h-16 flex ">
+          <div className="bg-[#051512]  h-16 w-4/5 flex  justify-between items-center px-3">
+            {/* logo */}
+            <div>
+              <p>Logo</p>
+            </div>
+            {/* link */}
+            <div className="flex gap-3 text-xs font-bold">
+              <Link>Home</Link>
+              <Link>All Product</Link>
+              <Link>Backpack</Link>
+              <Link>Climbing  Accessories</Link>
+            </div>
+          </div>
           <div className="bg-white opacity-10 w-1/5"></div>
         </div>
         {/* top nav end */}
-        {/* bottom nav */}
-        <div className="h-16 flex ">
-          <div className="w-4/5 bg-[#602101]"></div>
-          <div className="bg-white w-1/5">hi</div>
-        </div>
-        {/* bottom nav end */}
       </div>
     </Container>
   );
